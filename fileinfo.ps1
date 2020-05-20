db142c29d99fef08e2caa4c290b9c52ca2e1bbd1
softzcw <softzcw@hotmail.com>
2020-05-20 17:12:52 +0800

    Update fileinfo.ps1

--- fileinfo.ps1
+++ fileinfo.ps1
@@ -1,1 +1,11 @@
-
+##已知文件的路径，获取该文件所在的目录
+$SourceFilaPath = "$env:USERPROFILE\Desktop\VNC-Viewer-6.18.907-Windows-64bit.exe"
+$FileInfo = [System.IO.FileInfo]$SourceFilaPath
+##打印目录
+Write-Host ($FileInfo.DirectoryName)
+##打印该文件名,带后缀
+Write-Host ($FileInfo.Name)
+##打印该文件名,不带后缀
+Write-Host ($FileInfo.BaseName)
+##修改文件后缀
+write-host ([io.path]::Combine($FileInfo.DirectoryName, $file.BaseName) + '.png')
