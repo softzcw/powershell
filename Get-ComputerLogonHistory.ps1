0d779ca8f5dd1199361822495e88c5e0ac4f2f42
softzcw <softzcw@hotmail.com>
2022-01-26 15:56:26 +0800

    Create Get-ComputerLogonHistory.ps1
    通过windows事件日志获取通过历史登录ip

---
+++ Get-ComputerLogonHistory.ps1
@@ -0,0 +1,26 @@
+Function Get-ComputerLogonHistory{
+
+$XMLFilter='<QueryList>
+  <Query Id="0" Path="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational">
+    <Select Path="Microsoft-Windows-TerminalServices-LocalSessionManager/Operational">*[System[(EventID=21 or EventID=25)]]</Select>
+  </Query>
+</QueryList>'
+
+$Events=get-winevent -filterXml $XMLFilter -MaxEvents 1000
+
+            
+# Parse out the event message data            
+ForEach ($Event in $Events) {    
+
+      
+    # Convert the event to XML            
+    $eventXML = [xml]$Event.ToXml()     
+    # Append these as object properties            
+    Add-Member -InputObject $Event -MemberType NoteProperty -Force -Name  IP -Value $eventXML.Event.userdata.eventXML.address            
+              
+}            
+  
+    
+$events | select TimeCreated, ip
+#$events
+}
